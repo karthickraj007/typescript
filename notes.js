3d902db7a0af12efd6a8e344f767df60cf2e6e10
unknown <Karthickraj@CP-LP-206.credopay.com>
2025-12-20 14:31:04 +0530

    add new concepts

--- notes.js
+++ notes.js
@@ -106,13 +106,12 @@
         The asynchronous engine inside Node.js that runs the event loop, network I/O, timers, and thread pool.
 
     17)decorator
-        1)Decorators are special functions that modify/configure classes, methods, or properties, parameter.
-        2)TS
-            @decorator
-            class MyClass { }
-            
-        3)Js calls the decorator function like this
-            MyClass = decorator(MyClass);
+        1)A decorator is a function that attach to a class, method, or property.
+        It can:
+            Modify behavior
+            Add metadata
+            Wrap or replace functions
+            Log or track things
 
     18)metadata
         1)Extra information about your code that decorators attach
@@ -144,7 +143,7 @@
     21)Hoisting
         it is initializes memory for variables and functions before executing code.
 
-    22)Types of scopes -> rule for access variable
+    22)Types of scopes -> it decides where variables are accessible.
         1)global scope
             Variables accessible everywhere in your code 
 
@@ -157,52 +156,57 @@
         4)module scope
             Isolated scope where nothing is shared unless explicitly exported
 
-        5)Lexical Scope
-            1)variable is accessible based on where it is written in the code.
+        5)Lexical Scope ->Lexical Scope enables access
+            1)can access variables based on where it is written in the code.
+            1)it Allows inner function to access outer scope variable, but outer scopes cannot access inner scopes.
             2)lexical scope to build the scope hierarchy.
-            3)ex -> child scope can access parent scope
 
         6)Scope Chain
            1)Scope chain is the path for search variable
 
     22)execution Context
         1)execution context is like a "workspace" where your code runs
-        2)Types of Execution Context
-            1)Global Execution Context
-                1)global variables
-                2)global functions
-                3)window Object
+        2)each function needs its own memory space → this memory space is Function Execution Context (FEC).
+        3)When a function is called, only then JS creates a Function Execution Context for that function.
 
-            2)Function Execution Context
-                1)function arguments
-                2)local variables
-                3)local functions
+    23)Types of Execution Context
+        1)Global Execution Context
+            1)global variables
+            2)global functions
+            3)window Object
 
-            3)Module Execution Context
-                1)Created when module is imported
-                2)module variables
+        2)Function Execution Context
+            1)function arguments
+            2)local variables
+            3)local functions
 
-        3)execution Context has two things:
-            1)MEMORY CREATION PHASE
-               1.Create arguments: {a: undefined, b: undefined}
-               2.Hoist variables: x = undefined
-               3.Hoist function declarations and class
-               4.Setup scope chain
-               5.Set this value
-            2)EXECUTION PHASE
-                Now JS goes line by line and executes the code.
-                1)Assigns values
-                2)Runs functions
-                3)Evaluates expressions
-                4)Controls flow (if/for/etc)
-                5)Create CLOSURES when needed
+        3)Module Execution Context
+            1)Created when module is imported
+            2)module variables and module function 
 
-    23)closure
-        A closure is a function that remembers and can access variables from its outer scope, even after the outer function has finished execution.
+    24)execution Context has two things:
+        1)MEMORY CREATION PHASE
+            1.Create arguments: {a: undefined, b: undefined}
+            2.Hoist variables: x = undefined
+            3.Hoist function declarations and class
+            4.Setup scope and scope chain
+            5.Set this value
+        2)EXECUTION PHASE
+            Now JS goes line by line and executes the code.
+            1)Assigns values
+            2)Runs functions
+            3)Evaluates expressions
+            4)Controls flow (if/for/etc)
+            5)Create CLOSURES when needed
+
+    23)closure ->Closure enables memory
+        1Closure happens when a function remembers variables from its outer scope even after that outer scope has finished executing.
+        2)The function creates the closure
+        3)Closure is the memory that function keeps
 
     24)diff between tsc and ts-node
         Feature	             tsc	                            ts-node
-        1)Purpose	         Compile TS → JS	                Run TS directly
+        1)Purpose	         Compile TS → JS	                Run TS files directly
         2)JS Output	         Creates .js files on disk	        No files, JS in memory
         3)Execution	         Does not run code	                Runs TS immediately
         4)Type Checking	     Full type checking	                Default yes, can skip with --transpile-only
@@ -252,13 +256,13 @@
             import { m } from './main.js';
             m = 5; //Error in ESM
 
-    31)callstack
-        1)It's the mechanism that tracks function calls and Execution Contexts
+    31)callstack ->Last-In-First-Out
+        1)Data structure that manages execution contexts.
         2)It tracks:
             1)Which Execution Context is currently running
-            2)What function code is being executed
-            3)Where to return when done
-            4)The exact position in the code
+            2)which function is running now(via its execution context)
+            3)where to return after finishing
+            4)which function called which, and in what hierarchy
 
     32)diff between Variable Environment vs Lexical Environment
         1)Variable Environment
@@ -356,5 +360,115 @@
         1)npx is a package runner. Its main job is to execute packages.
 
     43)prototype
-        Prototype is a special hidden object that stores shared properties and methods for other objects.
+        1)Prototype is a special hidden object that stores shared properties and methods for other objects.
+        2)templates -> prototype exists
+            1)class
+            2)function
+        3)Instance / Object / Array → does NOT have .prototype
+        4)Instance → has __proto__ pointing to template’s .prototype
+
+    44)this -> keyword
+        1)reference to the current object
+        2)this depends on how a function is called.
+        3)this allows the same function to work with DIFFERENT objects 
+        4)Call style	this
+            obj.fn()	obj
+            new Fn()	new empty object {}
+            fn()	    window (non-strict mode) undefined (strict mode)
+
+    45)bind
+        1)bind() is a function that forces this keyword to point to a specific object.
+        2)creates a new function where this is permanently fixed to the object you pass.
+
+    46)call
+        Runs the function immediately and sets this temporarily.
+
+    47)apply
+        runs immediately, but arguments are passed as an array.
+
+    48)contex
+        Context is the current object that the function is working with (this points to that object).
+
+    49)memory management
+        1)Local variables are destroyed after function execution.
+        2)Properties on this live as long as the object exists.
+        3)object is no longer referenced anywhere, JS garbage collector frees memory for that object and its properties.
+
+    50)spread operator
+        1)it Expands an array/object into individual elements
+        2)spread operator works with any iterable.
+        3)ex -> string object array
+
+    51)rest operator
+        it Collects multiple arguments into an array
+
+    52)ts-node
+        it is tool to execute ts files directly
+
+    53)js execution order
+        1. JS sees fn()
+        2. fn() is pushed to Call Stack
+        3. V8 creates Execution Context for that function
+        4. Memory creation phase happens (Hoisting + variables etc.)
+        5. Execution phase starts (code runs)
+        6. Function finishes → EC removed from Call Stack
+
+    54)What is for?
+        1)for is a control-flow keyword in JavaScript.
+        2)It tells JavaScript:
+            Repeat this block of code again and again until the condition becomes false.
+
+    55)what is block
+        A block is just code written inside curly braces { }.
+
+    56)Order of steps in JS engine
+        1)Load Time
+            1)Memory Creation Phase / Variable Environment setup
+
+        2)Definition Time
+            1)Initialization / Execution Time
+
+    57)function
+        1)A function is just a special type of object.
+        2)Function → object + callable behavior
+        3)Types of function
+            1)Factory Function
+                A normal function that creates and returns an object.
+
+            2)Constructor Function
+                A special function that is used with new keyword to create an object
+
+            3)class is syntactic sugar for constructor functions
+
+    58)What is an instance?
+        1)instace is a object created from a “template” 
+        2)classes and functions are templates.
+
+    59)Webpack
+        1)Webpack is a tool that combine mutiple files to one file.(bundle)
+        2)It reads your project files and produces the bundle.
+        3)It handles:
+            Combining files
+            Minifying
+            Optimizing
+            Removing unused code
+            Processing CSS, images, etc.
+
+    60)Object.defineProperty()
+        1)it is a JavaScript method used to define a new property or modify an existing property
+        2)it allows you to control the property’s behavior.
+
+    61)placeholder
+        1)Placeholder is a temporary name that will be replaced later
+        2)Ex -> <h1 placeholder="hello"> </h1>
+
+    62)Inheritance
+        1)One object can use properties or methods of another object
+        2)The object doesn’t have the property itself, but it can still access it through the prototype chain.
+
+    63)extends vs implements
+        1)A class uses extends only to inherit another class, 
+        2)A class uses implements to follow an interface.
+
+
 */
